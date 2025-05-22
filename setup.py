a52dfb77fbc4b3ac2b19a65fb75cc03001c622d5
Winonymous <anjuwonololade@gmail.com>
2025-05-23 00:01:21 +0100

    Update setup.py to refine package configuration and dependencies

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'yahboom_camera_controller'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'tf2_ros', 'geometry_msgs'],
     zip_safe=True,
     maintainer='lolade',
     maintainer_email='anjuwonololade@gmail.com',
